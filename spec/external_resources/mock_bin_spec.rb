84e79f75918a83f4db57f1be82cc1179b47154d1
Isaiev <webisaev@gmail.com>
2019-03-30 13:49:50 +0200

    add some tests

--- spec/external_resources/mock_bin_spec.rb
+++ spec/external_resources/mock_bin_spec.rb
@@ -14,14 +14,23 @@ describe ExternalResources::MockBin do
     end
   end
 
-  describe '.top_level_key' do
+  describe '.representer' do
     it 'has method top_key' do
-      expect(external_resource).to respond_to(:top_level_key)
+      expect(external_resource).to respond_to(:representer)
     end
 
-    it 'is a string' do
-      expect(external_resource.url).to be_a(String)
+    it 'is has method #from_hash' do
+      expect(external_resource.representer.method_defined?(:from_hash)).to eq(true)
     end
+  end
+
+  describe '.parse_response' do
+    it 'correctly parses response' do
+      response_body = { ads: [{ a: 1 }] }
+
+      expect(external_resource.parse_response(response_body.to_json)).to eq([{ 'a' => 1 }])
+    end
+
   end
 
 end
